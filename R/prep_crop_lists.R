803545c4d3330c41bffcb69b17ec9e95e6d9e2d4
emilysturdivant <emilysturdivant@gmail.com>
2021-05-13 15:58:35 -0500

    rework crop to pollinator pre-processing

--- R/prep_crop_lists.R
+++ R/prep_crop_lists.R
@@ -2,82 +2,144 @@
 
 
 # Load libraries
-library('rgbif')
 library('tidyverse')
-
-
-
 
 # ----
 # Fxn to standardize crop species names 
-standardize_species <- function(x) {
+standardize_crop_species <- function(x) {
   str_trim(x) %>% 
+    str_replace_all("\\s+", " ") %>% 
     str_to_sentence() %>% 
     str_replace_all('frutescen\\b', 'frutescens') %>% 
     str_replace_all('\\bannum\\b', 'annuum') %>% 
     str_replace_all('\\bparadisii\\b', 'paradisi') %>% 
     str_replace_all('Musa paradisiaca', 'Musa x paradisiaca') %>% 
     str_replace_all('\\bficus-.*indica\\b', 'ficus-indica') %>% 
-    str_replace_all('pecten-.*aboriginum', 'pecten-aboriginum') %>% 
+    str_replace_all(regex('pecten-.*aboriginum', ignore_case = TRUE), 
+                    'pecten-aboriginum') %>% 
     str_replace_all('\\bliebmanii\\b', 'liebmannii') %>% 
-    str_replace_all('Solanum lycopersicum \\(lycopersicon esculentum\\)', 'Lycopersicon esculentum') %>% 
+    str_replace_all('Solanum lycopersicum \\(lycopersicon esculentum\\)', 'Solanum lycopersicum') %>% 
+    str_replace_all('Lycopersicon esculentum', 'Solanum lycopersicum') %>% 
+    str_replace_all('Citrus (?=limon|aurantifolia|latifolia|tangelo)', 'Citrus x ') %>% 
+    str_replace_all('Citrus x (?=paradisi|sinensis|reticulata)', 'Citrus ') %>% 
+    str_replace_all('Mentha spicata l\\.', 'Mentha spicata') %>% 
+    str_replace_all('Mentha viridis', 'Mentha spicata') %>% 
+    str_replace_all('Nopalxochia phyllantoides', 'Nopalxochia phyllanthoides') %>% 
+    str_replace_all('Lecythis sp\\.', 'Lecythis usitata') %>% 
+    str_replace_all('Luma apiculata', 'Psidium sartorianum') %>% 
+    str_replace_all('Macadamia .*', 'Macadamia spp.') %>% 
+    str_replace_all('Mastichodendron .*', 'Sideroxylon palmeri') %>% 
     str_remove_all('\\(.*\\)') %>% 
+    str_replace_all("\\s+", " ") %>% 
     str_trim()
 }
 
-# Load all crop tables data ----
-# Apendice2, crop to pollinator table
-crp_pllntrs_fp <- 'data/tidy/Quesada/crop_pllntrs_from_appendix2.csv'
-ap2_df <- read_csv(crp_pllntrs_fp) %>% 
-  mutate(Cultivo = standardize_species(Cultivo))
-crops_ap2 <- ap2_df %>% 
-  distinct(Cultivo)
+# Apendice1, crop production values and importance of pollination ----
+# DD = uso directo y dependiente de polinizador
+ap1_xls_fp <- 'data/input_data/Quesada_bioclim_pol_y_cultivos/appendices/Apendice1.xlsx'
+ap1_sheets <- list(
+  readxl::read_excel(ap1_xls_fp, na = c('- -', '.'), .name_repair = 'universal'),
+  readxl::read_excel(ap1_xls_fp, 2, na = c('- -', '.'), .name_repair = 'universal'),
+  readxl::read_excel(ap1_xls_fp, 3, na = c('- -', '.'), .name_repair = 'universal'),
+  readxl::read_excel(ap1_xls_fp, 4, na = c('- -', '.'), .name_repair = 'universal'))
+
+ap1_df <- plyr::join_all(ap1_sheets, by = 'Especie', type = 'full')
+ap1_df <- ap1_df %>% mutate(Cultivo = standardize_crop_species(Especie))
+crops_ap1 <- ap1_df %>% select(Cultivo)
+
+# Get crops with highest pollinator value (Valor del polinizador) 
+top10_ap1 <- ap1_df %>% 
+  filter(str_detect(Importancia.de.la.polinización, 'DD|D\\?')) %>%
+  arrange(desc(Valor.del.polinizador.en.2010..MEX..)) %>% 
+  head(10) 
+
+# Apendice2, crop to pollinator table ----
+crp_pllntrs_fp <- 'data/tidy/Quesada/crop_pllntrs_from_ap2_updated.csv'
+ap2_df <- tidy_croppllntrs_tbl(crp_pllntrs_fp) %>% 
+  mutate(Cultivo = standardize_crop_species(Cultivo))
+
+# Extract species of top crops from ap2
+top10_pols <- semi_join(ap2_df, top10_ap1, by = "Cultivo")
+top10_pols %>% distinct(Cultivo)
+
+# Look at genera listed for each crop
+crop_list <- top10_pols %>% 
+  group_by(Cultivo) %>% 
+  distinct(genus)
+
+
+# Join ap1 to ash_df and narrow to high-priority crops ----
+# Which crops will be left out?
+ap1_missing <- anti_join(ash_df, ap1_df, by = 'Cultivo')
+ash_missing <- anti_join(ap1_df, ash_df, by = 'Cultivo')
+
+# Join Ashworth to Apendice1
+ap1_ash <- left_join(ap1_df, ash_df, by = 'Cultivo')
+
+# Variables: 
+ap1_ash %>% tbl_vars
+dependent_df <- ap1_ash %>% 
+  filter(str_detect(Importancia.de.la.polinización, 'DD|D\\?'))
+
+# Get 20 crops with highest pollinator value (Valor del polinizador)
+top10_ap1 <- dependent_df %>%
+  arrange(desc(Valor.del.polinizador.en.2010..MEX..)) %>% 
+  head(10) %>% 
+  select(Cultivo, Nombre.común.en.español, Importancia.de.la.polinización, 
+         `Level of pollinator dependence`, Valor.del.polinizador.en.2010..MEX.., 
+         Valor.de.producción.en.2010..MEX.., 
+         Valor.de.producción.esperada.al.2050.según.cambio.climático..MEX..)
+
+# Extract species of top crops from ap2
+top10_ap1 %>% anti_join(ap2_df, by = "Cultivo") %>% nrow
+top10_pols <- semi_join(ap2_df, top10_ap1, by = "Cultivo")
+
+# Look at genera listed for each crop
+crop_list <- top10_pols %>% 
+  group_by(Cultivo) %>% 
+  distinct(genus)
+crop <- crop_list[[1]]
+
+top10_pols %>% distinct(Cultivo)
+
 
 # Ashworth crop pollinator dependence, crop species, common names, dependence on pollinators
+# E: essential (pollinator absence produce a reduction of > 90% of crop production compared with pollinator presence), 
+# H: high (40-< 90% of reduction in absence of pollinators), 
+# M: modest (10-< 40% of reduction), 
+# L: little (>0-< 10 % of reduction),
+# NI: no increase (no yield increase with animal mediated pollination).
 ashworth_fp <- 'data/input_data/Quesada_bioclim_pol_y_cultivos/BaseCultivosAsworth_MXname.csv'
 ash_df <- read_delim(ashworth_fp, delim = ';') %>% 
-  mutate(Cultivo = standardize_species(`Scientific name`))
+  mutate(Cultivo = standardize_crop_species(`Scientific name`))
 crops_ash <- ash_df %>% select(Cultivo)
 
 # Informacion_general, crop biological information
 igcult_fp <- 'data/input_data/Quesada_bioclim_pol_y_cultivos/Info_general_cultivos.csv'
 igcult_df <- read_delim(igcult_fp, delim = ';', skip = 1) %>% 
-  mutate(Cultivo = standardize_species(Especie))
+  mutate(Cultivo = standardize_crop_species(Especie))
 crops_igcult <- igcult_df %>% 
   select(Cultivo)
-
-# Apendice1, crop production values and importance of pollination
-# DD = uso directo y dependiente de polinizador
-ap1_xls_fp <- 'data/input_data/Quesada_bioclim_pol_y_cultivos/appendices/Apendice1.xlsx'
-ap1_sheets <- list(
-  readxl::read_excel(ap1_xls_fp, na = '- -'),
-  readxl::read_excel(ap1_xls_fp, 2, na = '- -'),
-  readxl::read_excel(ap1_xls_fp, 3, na = '- -'),
-  readxl::read_excel(ap1_xls_fp, 4, na = '- -'))
-
-ap1_df <- plyr::join_all(ap1_sheets, by = 'Especie', type = 'full')
-ap1_df <- ap1_df %>% mutate(Cultivo = standardize_species(Especie))
-crops_ap1 <- ap1_df %>% select(Cultivo)
 
 # compare to Info general
 anti_join(crops_ap1, crops_igcult)
 anti_join(crops_igcult, crops_ap1)
 
+# Compare the four crop lists ----
 # merge
 bound <- bind_rows(
-  mutate(crops_ap2, source='ap2'),
+  mutate(distinct(ap2_df, Cultivo), source='ap2'),
   mutate(crops_ash, source = 'ashworth'), 
   mutate(crops_igcult, source = 'info_general'), 
-  mutate(crops_ap1, source = 'info_general')
+  mutate(crops_ap1, source = 'ap1')
 ) %>% 
   arrange(Cultivo)
 
 bound2 <- bound %>% group_by(Cultivo) %>% count() %>% ungroup %>% 
   arrange(Cultivo)
-length(bound2[27,1] %>% deframe)
 
 
-
+# Random comparisons
 
 ash_missing <- anti_join(crops_ash, crops_ap2)
 anti_join(ash_missing, crops_igcult)
@@ -92,9 +154,8 @@ anti_join(crops_ap2, crops_igcult)
 
 
 
-# Join ap2 to ash_df and narrow to high-priority crops ----
+# Join ap2 to ash_df and narrow to high-priority crops 
 # Which crops will be left out?
 ashworth_wo_pllntrs <- anti_join(ash_df, ap2_df, by = 'Cultivo')
 
 ashworth_pollinators <- left_join(ash_df, ap2_df, by = 'Cultivo')
-
---
+++ R/prep_crop_pllntrs.R
@@ -0,0 +1,118 @@
+# Create tidy DF matching crops to pollinator species
+# convert table from Apendice2, shared by Dr. Quesada 
+
+# Load libraries
+library("tidyverse")
+
+# Functions ----
+tidy_croppllntrs_tbl <- function(in_fp, out_fp=NULL) {
+  
+  # Load table and propogate crop and family down 
+  df <- read_csv(in_fp, trim_ws=T) %>% 
+    fill(Familia, Cultivo)
+  
+  # Extract taxonomic classification from the pollinator variable
+  df <- df %>% 
+    mutate(Polinizador = str_trim(Polinizador),
+           species = Polinizador %>% 
+             str_remove_all("s. str.") %>% 
+             str_remove_all("\\(.*\\)") %>% 
+             str_replace_all("\\s+", " ") %>% 
+             str_extract("^[:upper:]\\w*\\s\\w*"),
+           genus = str_extract(Polinizador, '^[:upper:]\\w*') %>% 
+             str_remove_all("[:punct:]"), # assume first word is genus if starts with capital letter
+           subgenus = Polinizador %>% 
+             str_extract('(?<=\\()[:upper:]\\w+\\b'),
+           family = str_extract(Grupo, '\\w*idae'),
+           order = str_extract(Grupo, '\\w*ptera|\\w*formes'), 
+           class = case_when(
+             str_detect(Grupo, regex('aves', ignore_case = TRUE)) |
+               str_detect(Polinizador, regex('aves', ignore_case = TRUE)) ~ 'Aves',
+             str_detect(Polinizador, regex('insectos', ignore_case = TRUE)) ~ 'Insecta',
+             TRUE ~ as.character(NA)), 
+           common_group = case_when(
+             str_detect(Polinizador, regex('abeja', ignore_case = TRUE)) ~ 'Abejas', 
+             str_detect(Polinizador, regex('colibri', ignore_case = TRUE)) ~ 'Colibries', 
+             str_detect(Polinizador, regex('mariposa', ignore_case = TRUE)) ~ 'Mariposas', 
+             str_detect(Polinizador, regex('murci.+lago', ignore_case = TRUE)) ~ 'Murcielagos', 
+             str_detect(Polinizador, regex('avispa', ignore_case = TRUE)) ~ 'Avispas', 
+             str_detect(Polinizador, regex('mosca', ignore_case = TRUE)) ~ 'Moscas',
+             TRUE ~ as.character(NA)),
+           tribe = case_when(
+             str_detect(Polinizador, regex('abejas sin aguij.+n', ignore_case = TRUE)) ~ 'Meliponini',
+             TRUE ~ as.character(NA)),
+           subgroup_a = case_when(
+             str_detect(Polinizador, regex('abejas solitarias', ignore_case = TRUE)) ~ 'Solitaria',
+             str_detect(Polinizador, regex('abejas sociales', ignore_case = TRUE)) ~ 'Social',
+             TRUE ~ as.character(NA)))
+  
+  # Save file
+  if(is.character(out_fp)) write_csv(df, out_fp)
+  
+  # Return
+  return(df)
+}
+
+# Fxn to standardize crop species names 
+standardize_crop_species <- function(x) {
+  str_trim(x) %>% 
+    str_replace_all("\\s+", " ") %>% 
+    str_to_sentence() %>% 
+    str_replace_all('frutescen\\b', 'frutescens') %>% 
+    str_replace_all('\\bannum\\b', 'annuum') %>% 
+    str_replace_all('\\bparadisii\\b', 'paradisi') %>% 
+    str_replace_all('Musa paradisiaca', 'Musa x paradisiaca') %>% 
+    str_replace_all('\\bficus-.*indica\\b', 'ficus-indica') %>% 
+    str_replace_all(regex('pecten-.*aboriginum', ignore_case = TRUE), 
+                    'pecten-aboriginum') %>% 
+    str_replace_all('\\bliebmanii\\b', 'liebmannii') %>% 
+    str_replace_all('Solanum lycopersicum \\(lycopersicon esculentum\\)', 'Solanum lycopersicum') %>% 
+    str_replace_all('Lycopersicon esculentum', 'Solanum lycopersicum') %>% 
+    str_replace_all('Citrus (?=limon|aurantifolia|latifolia|tangelo)', 'Citrus x ') %>% 
+    str_replace_all('Citrus x (?=paradisi|sinensis|reticulata)', 'Citrus ') %>% 
+    str_replace_all('Mentha spicata l\\.', 'Mentha spicata') %>% 
+    str_replace_all('Mentha viridis', 'Mentha spicata') %>% 
+    str_replace_all('Nopalxochia phyllantoides', 'Nopalxochia phyllanthoides') %>% 
+    str_replace_all('Lecythis sp\\.', 'Lecythis usitata') %>% 
+    str_replace_all('Luma apiculata', 'Psidium sartorianum') %>% 
+    str_replace_all('Macadamia .*', 'Macadamia spp.') %>% 
+    str_replace_all('Mastichodendron .*', 'Sideroxylon palmeri') %>% 
+    str_remove_all('\\(.*\\)') %>% 
+    str_replace_all("\\s+", " ") %>% 
+    str_trim()
+}
+
+# Filepaths ----
+ap1_xls_fp <- 'data/input_data/Quesada_bioclim_pol_y_cultivos/appendices/Apendice1.xlsx'
+crp_pllntrs_fp <- 'data/tidy/Quesada/crop_pllntrs_from_ap2_updated.csv'
+
+# Apendice1, crop production values and importance of pollination ----
+# DD = uso directo y dependiente de polinizador
+ap1_sheets <- list(
+  readxl::read_excel(ap1_xls_fp, na = c('- -', '.'), .name_repair = 'universal'),
+  readxl::read_excel(ap1_xls_fp, 2, na = c('- -', '.'), .name_repair = 'universal'),
+  readxl::read_excel(ap1_xls_fp, 3, na = c('- -', '.'), .name_repair = 'universal'),
+  readxl::read_excel(ap1_xls_fp, 4, na = c('- -', '.'), .name_repair = 'universal'))
+
+ap1_df <- plyr::join_all(ap1_sheets, by = 'Especie', type = 'full')
+ap1_df <- ap1_df %>% mutate(Cultivo = standardize_crop_species(Especie))
+crops_ap1 <- ap1_df %>% select(Cultivo)
+
+# Get crops with highest pollinator value (Valor del polinizador) 
+top10_ap1 <- ap1_df %>% 
+  filter(str_detect(Importancia.de.la.polinización, 'DD|D\\?')) %>%
+  arrange(desc(Valor.del.polinizador.en.2010..MEX..)) %>% 
+  head(10) 
+
+# Apendice2, crop to pollinator table ----
+ap2_df <- tidy_croppllntrs_tbl(crp_pllntrs_fp) %>% 
+  mutate(Cultivo = standardize_crop_species(Cultivo))
+
+# Extract species of top crops from ap2
+top10_pols <- semi_join(ap2_df, top10_ap1, by = "Cultivo")
+top10_pols %>% distinct(Cultivo)
+
+# Look at genera listed for each crop
+crop_list <- top10_pols %>% 
+  group_by(Cultivo) %>% 
+  distinct(genus)
